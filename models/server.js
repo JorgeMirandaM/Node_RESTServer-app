36954022ba86f047ff5d5c2b9a772cff7376c171
JorgeMirandaM <jorge100099hotmail.com>
2023-11-03 21:16:43 -0600

    routes, controllers and cors implemented

--- models/server.js
+++ models/server.js
@@ -1,10 +1,11 @@
 const express= require('express');
-
+const cors = require('cors');
 class Server {
 
     constructor(){
         this.app=express();
         this.port=process.env.PORT;
+        this.usuariosPath='/api/usuarios';
 
         // Middlewares
         this.middleware();
@@ -15,17 +16,19 @@ class Server {
     }
 
     middleware(){
+
+        //CORS
+        this.app.use(cors());
+
         //Directorio publico
         this.app.use(express.static('public'));
 
     }
 
     routes(){
-        this.app.get('/',(req,res)=>{
-            res.send('Hola mundo')
-        })
+        this.app.use(this.usuariosPath,require('../routes/usuarios'));
     }
-
+ 
     listen(){
         this.app.listen(this.port,()=>{
             console.log(`Server running on port ${this.port}`);
