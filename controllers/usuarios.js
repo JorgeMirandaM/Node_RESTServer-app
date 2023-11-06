648761aebaab3d7b54273eddfd6f0a80e8a7f2bc
JorgeMirandaM <jorge100099hotmail.com>
2023-11-05 21:06:10 -0600

    validation if email exists function implemented

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -20,14 +20,7 @@ const usuariosPost=async (req,res=response)=>{
     const {nombre,correo, password, rol}= req.body;
 
     const usuario= new Usuario({nombre,correo, password, rol});
-
-    //Verificar si el correo existe
-    const existeEmail= await Usuario.findOne({correo:correo});
-    if(existeEmail) {
-        return res.status(400).json({
-            msg: 'El correo ya está registrado'
-        })
-    }
+    
 
     //Encriptar la contraseña
     const salt= bcryptjs.genSaltSync();
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,10 +1,9 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const Role= require('../models/role');
 
 const { usuariosGet, usuariosPost, usuariosPut, usuariosPatch, usuariosDelete } = require('../controllers/usuarios');
 const { validarCampos } = require('../middlewares/validar-campos');
-const { esRoleValido } = require('../helpers/db-validators');
+const { esRoleValido, emailExiste } = require('../helpers/db-validators');
 
 const router = Router();
 
@@ -15,11 +14,12 @@ router.post('/', [
     check('password', 'El password debe de ser más de 6 letras').isLength({ min: 6 }),
     check('correo', 'El correo no es válido').isEmail(),
     // check('rol','No es un rol válido').isIn(['ADMIN_ROLE','USER_ROLE']),
-    check('rol').custom((rol)=>esRoleValido(rol)),
+    check('rol').custom((rol) => esRoleValido(rol)),
+    check('correo').custom((correo) => emailExiste(correo)),
     validarCampos
 ], usuariosPost),
 
-    router.put('/:id', usuariosPut);
+router.put('/:id', usuariosPut);
 router.patch('/', usuariosPatch);
 router.delete('/', usuariosDelete);
 
