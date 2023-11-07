b752f94c3fd8aee4c828950da4787b9fe1b3356e
JorgeMirandaM <jorge100099hotmail.com>
2023-11-07 12:18:00 -0600

    return total number of registers in a collection

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,17 +1,25 @@
-const {request, response } = require('express');
+const { request, response } = require('express');
 const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 
-const usuariosGet = async(req=request, res = response) => {
+const usuariosGet = async (req = request, res = response) => {
 
-    const {limite=5, desde=0 }= req.query;
-    const usuarios= await Usuario.find()
-    .skip(Number(desde))
-    .limit(Number(limite));
+    const { limite = 5, desde = 0 } = req.query;
+
+    const query = { estado: true }
+
+
+    const [total,usuarios] =await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+            .skip(Number(desde))
+            .limit(Number(limite))
+    ]);
 
     res.json({
         msg: 'get API - controlador',
+        total,
         usuarios
     })
 }
@@ -42,7 +50,7 @@ const usuariosPost = async (req, res = response) => {
 const usuariosPut = async (req, res = response) => {
 
     const { id } = req.params;
-    const { _id,password, google, correo, ...resto } = req.body;
+    const { _id, password, google, correo, ...resto } = req.body;
 
     if (password) {
         //Encriptar la contraseña
