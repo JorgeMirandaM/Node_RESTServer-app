726ed66da200151c6c07b59a3c018aed7689f48e
JorgeMirandaM <jorge100099hotmail.com>
2023-11-07 19:29:48 -0600

    auth controller created

---
+++ controllers/auth.js
@@ -0,0 +1,48 @@
+const Usuario= require('../models/usuario');
+const bcryptjs= require('bcryptjs');
+
+const login =async (req, res) => {
+    const { correo, password } = req.body;
+
+    try {
+
+
+        //Verificar si el email existe
+        const usuario= await Usuario.findOne({correo});
+        if(!usuario){
+            return res.status(400).json({
+                msg:'Usuario / Password no son correctos - correo'
+            })
+        }
+
+        // Si el usuario está activo
+        if(!usuario.estado){
+            return res.status(400).json({
+                msg:'Usuario / Password no son correctos - estado: false'
+            })
+        }
+
+        //Verificar la contraseña
+        const validPassword = bcryptjs.compareSync(password,usuario.password);
+        if(!validPassword){
+            return res.status(400).json({
+                msg:'Usuario / Password no son correctos - password'
+            })
+        }
+
+        res.json({
+            msg: 'Login ok'
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg:'Hable con el administrador'
+        })
+    }
+
+}
+
+module.exports = {
+    login
+}
