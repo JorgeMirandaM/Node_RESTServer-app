7732eca91ce96ded8974ed0078e19e45e8c3671d
JorgeMirandaM <jorge100099@hotmail.com>
2023-11-12 14:46:21 -0600

    fileUpload function as helper created

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,46 +1,24 @@
-const path= require('path');
+const { subirArchivo } = require('../helpers');
 
-const {v4:uuidv4} = require('uuid');
 
-const cargarArchivo = (req,res)=>{
+const cargarArchivo =async (req,res)=>{
 
-    let sampleFile;
-    let uploadPath;
   
     if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
       res.status(400).json({msg:'No hay archivos que subir'});
       return;
     }
 
+
+    const nombre= await subirArchivo(req.files);
+
+    res.json({
+      nombre
+    })
    
   
   
-    const {archivo} = req.files;
-
-    const nombreCortado= archivo.name.split('.');
-    const extension= nombreCortado[nombreCortado.length - 1];
-
-    //Validar la extension
-    const extensionesValidas= ['png','jpg','jpeg','gif'];
-
-    if(!extensionesValidas.includes(extension)){
-      return res.status(400).json({
-        msg:`La extensión ${extension} no es permitida, ${extensionesValidas}`
-      })
-    }
-
-
-    
-    const nombreTemp= uuidv4() + '.' + extension;
-    uploadPath =path.join(__dirname,'../uploads/',nombreTemp);
-  
-    archivo.mv(uploadPath, (err)=> {
-      if (err) {
-        return res.status(500).json({err});
-      }
-  
-      res.json({msg:'File uploaded to ' + uploadPath});
-    });
+   
 
 }
 
