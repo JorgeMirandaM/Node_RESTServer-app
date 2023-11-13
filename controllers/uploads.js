64d05db4064f97ee2f56ee30c337726df8682d2d
JorgeMirandaM <jorge100099@hotmail.com>
2023-11-13 12:41:59 -0600

    update image function implemented

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,41 +1,63 @@
 const { subirArchivo } = require('../helpers');
 
-
-const cargarArchivo =async (req,res)=>{
-
-  
-    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
-      res.status(400).json({msg:'No hay archivos que subir'});
-      return;
-    }
+const { Usuario, Producto } = require('../models');
 
 
-    try {
-      const nombre= await subirArchivo(req.files,undefined,'imgs');
+const cargarArchivo = async (req, res) => {
+
+  try {
+    const nombre = await subirArchivo(req.files, undefined, 'imgs');
 
     res.json({
       nombre
     })
-   
-    } catch (error) {
-      res.status(400).json({error});
-    }
+
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+
+}
+
+
+
+const actualizarImagen = async (req, res) => {
+
+  const { id, coleccion } = req.params;
+
+  let modelo;
+
+  switch (coleccion) {
+    case 'usuarios':
+      modelo = await Usuario.findById(id);
+      if (!modelo) {
+        return res.status(400).json({
+          msg: `No existe un usuario con el id ${id}`
+        });
+      }
+      break;
+
+    case 'productos':
+      modelo = await Producto.findById(id);
+      if (!modelo) {
+        return res.status(400).json({
+          msg: `No existe un producto con el id ${id}`
+        });
+      }
+      break;
+
+    default:
+      return res.status(500).json({ msg: 'Se me olvidó validar esto' })
+  }
+
+  const nombre = await subirArchivo(req.files, undefined, coleccion);
+  modelo.img=nombre;
   
-  
-   
+  await modelo.save();
 
+  res.json(modelo);
 }
 
-
-
-const actualizarImagen= async(req,res)=>{
-
-  const {id,coleccion }= req.params;
-
-  res.json({id,coleccion});
-}
-
-module.exports={
-    cargarArchivo,
-    actualizarImagen
-}
+module.exports = {
+  cargarArchivo,
+  actualizarImagen
+}  
--- routes/uploads.js
+++ routes/uploads.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check, query } = require('express-validator');
-const { validarCampos } = require('../middlewares/validar-campos');
+const { validarCampos,validarArchivoSubir } = require('../middlewares');
 const { cargarArchivo, actualizarImagen } = require('../controllers/uploads');
 const { coleccionesPermitidas } = require('../helpers');
 
@@ -10,9 +10,10 @@ const router = Router();
 router.post('/',cargarArchivo);
 
 router.put('/:coleccion/:id',[
+    validarArchivoSubir,
     check('id','El id debe ser de mongo').isMongoId(),
     check('coleccion').custom(c=> coleccionesPermitidas(c,['usuarios','productos'])),
     validarCampos
-],actualizarImagen)
+],actualizarImagen);
 
 module.exports= router;
